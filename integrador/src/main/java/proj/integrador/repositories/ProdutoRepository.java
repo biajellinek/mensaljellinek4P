013fa75749748dc3a29a4fadb875fa64223e2797
biajellinek <biancajellinek1811@gmail.com>
2024-09-24 11:48:59 -0300

    produtoR

--- integrador/src/main/java/proj/integrador/repositories/ProdutoRepository.java
+++ integrador/src/main/java/proj/integrador/repositories/ProdutoRepository.java
@@ -1,13 +1,18 @@
 package proj.integrador.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import proj.integrador.entities.ProdutoEntity;
+import proj.integrador.entities.RankingEntity;
+import proj.integrador.services.ProdutoService;
 
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<ProdutoEntity,Integer>{
 
-	
+	Optional<ProdutoService> saveProdutos(ProdutoEntity produtoEntity);
+
 }
