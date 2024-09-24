edd0ad2c8bb4505c9078a5e9a14d0102cd528952
biajellinek <biancajellinek1811@gmail.com>
2024-09-24 11:47:32 -0300

    produto

--- integrador/src/main/java/proj/integrador/services/ProdutoService.java
+++ integrador/src/main/java/proj/integrador/services/ProdutoService.java
@@ -1,15 +1,22 @@
 package proj.integrador.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import proj.integrador.entities.AtleticaEntity;
 import proj.integrador.entities.ProdutoEntity;
+import proj.integrador.entities.RankingEntity;
+import proj.integrador.repositories.AtleticaRepository;
 import proj.integrador.repositories.ProdutoRepository;
 
 @Service
 public class ProdutoService { 
+	
+	@Autowired
+	private AtleticaRepository atleticaRepository;
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
@@ -30,5 +37,25 @@ public class ProdutoService {
 	public List<ProdutoEntity> findAll(){
 		return produtoRepository.findAll();
 }
+	
+	public boolean isDono (Long dono_id, Long atletica_id) {
+		
+		Optional<AtleticaEntity> atleticaOpt = atleticaRepository.findAll(atletica_id);
+		if(atleticaOpt.isPresent()) {
+			
+			return atleticaOpt.get().getDonoId().equals(dono_id);
+		}
+		return false;
+	}
+	
+	public ProdutoEntity salvarPostagem(ProdutoEntity produtos, Long dono_id, Long atletica_id) {
+		
+		if (isDono(dono_id, atletica_id)) {
+			return produtoRepository.save(produtos);
+			
+		} else {
+			throw new IllegalArgumentException("apenas o dono podera fazer essa acao");
+		}
+	}
 
 }
