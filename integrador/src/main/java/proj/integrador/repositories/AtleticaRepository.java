a8109f2e90c6a97d1ee1e79607932f72940ce02d
biajellinek <biancajellinek1811@gmail.com>
2024-09-24 11:49:31 -0300

    atketicaR

--- integrador/src/main/java/proj/integrador/repositories/AtleticaRepository.java
+++ integrador/src/main/java/proj/integrador/repositories/AtleticaRepository.java
@@ -1,14 +1,19 @@
 package proj.integrador.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import proj.integrador.entities.AtleticaEntity;
+import proj.integrador.entities.RankingEntity;
 
 
 @Repository
 public interface AtleticaRepository extends JpaRepository<AtleticaEntity,Integer>{
 	
-	
+	Optional<AtleticaEntity> findById(Long atletica_id);
+
+	Optional<AtleticaEntity> findAll(Long atletica_id);
 
 }
