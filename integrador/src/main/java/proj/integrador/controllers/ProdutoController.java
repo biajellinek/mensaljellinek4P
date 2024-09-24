fea6e10d33e2ad72b307f59b58b99f1de94ff39c
biajellinek <biancajellinek1811@gmail.com>
2024-09-24 11:52:02 -0300

    produtoC

--- integrador/src/main/java/proj/integrador/controllers/ProdutoController.java
+++ integrador/src/main/java/proj/integrador/controllers/ProdutoController.java
@@ -3,12 +3,14 @@ package proj.integrador.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import proj.integrador.entities.ProdutoEntity;
@@ -46,6 +48,16 @@ public class ProdutoController {
 	} catch(Exception e) {
 		return ResponseEntity.badRequest().build();
 	}
+	}
+	
+	@PostMapping("/novaPostagem")
+	public ResponseEntity<ProdutoEntity> criarPostagem(@RequestBody ProdutoEntity produto,@RequestParam Long dono_id, @RequestParam Long atletica_id){
+		try {
+			ProdutoEntity novaPostagem = produtoService.salvarPostagem(produto, dono_id, atletica_id);
+			return new ResponseEntity<> (novaPostagem, HttpStatus.CREATED);
+		} catch (IllegalArgumentException e) {
+			return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+		}
 	}
 
 }
