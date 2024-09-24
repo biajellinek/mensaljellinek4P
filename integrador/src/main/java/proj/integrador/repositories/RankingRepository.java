a9e3acf80f866b96b4b0a6622753f6296ce1658c
biajellinek <biancajellinek1811@gmail.com>
2024-09-24 11:48:02 -0300

    repository

--- integrador/src/main/java/proj/integrador/repositories/RankingRepository.java
+++ integrador/src/main/java/proj/integrador/repositories/RankingRepository.java
@@ -1,5 +1,6 @@
 package proj.integrador.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,8 @@ import proj.integrador.entities.RankingEntity;
 @Repository
 public interface RankingRepository extends JpaRepository<RankingEntity, Integer>{
 	
-	Optional<RankingEntity> findByAtleticaId(Integer atleticaId);
+	Optional<RankingEntity> findById(Integer atletica_id);
 
+	List<RankingEntity> findAllByPontuacaoTotalDesc();
 }
+
